a1c39ca9a5fda81fa8b66f1c80fd95b1bf0c947f
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-17 20:55:37 +0200

    debut TP3

--- Accueil.php
+++ Accueil.php
@@ -1,3 +1,14 @@
-<li><a href="formulaire.php">Formulaire inscription</a></li>
-<li><a href='login.php?step=LOGIN'>Login</a></li>
-<li><a href='logOK.php'>LoginOK</a></li>
+<?php
+        $rub1 ='Formulaire';
+        $rub1addr ='formulaire.php';
+
+        $rub2 ='LOGIN';
+        $rub2addr ='login.php?step=LOGIN';
+
+        $rub3 ='logOK';
+        $rub3addr ='logOK.php';
+?>
+
+<a href="<?php echo $rub1addr; ?>"><?php echo $rub1; ?> </a>
+<a href="<?php echo $rub2addr; ?>"><?php echo $rub2; ?> </a>
+<a href="<?php echo $rub3addr; ?>"><?php echo $rub3; ?> </a>
